7fc063e7ee60ab5711e6a7a8b50a4c50f4f3b6dd
Buğra Gündüz <bgrgndzz@gmail.com>
2018-08-11 16:05:46 +0300

    make the landing page responsive

--- public/js/index.js
+++ public/js/index.js
@@ -6,3 +6,24 @@ window.onscroll = () => {
     headerLogo.style.opacity = 0;
   }
 };
+window.onload = () => {
+  const anchor = document.querySelector('a[href^="#nasil"]');
+  anchor.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('#nasil').scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+  const testimonials = document.querySelectorAll('.testimonial');
+  let currentTestimonial = 0;
+  setInterval(() => {
+    testimonials[currentTestimonial].classList.remove('fadeInRight');
+    testimonials[currentTestimonial].classList.add('fadeOutLeft');
+    currentTestimonial = currentTestimonial === 2 ? 0 : currentTestimonial + 1;
+    setTimeout(() => {
+      testimonials[currentTestimonial].classList.remove('testimonial__hidden');
+      testimonials[currentTestimonial].classList.remove('fadeOutLeft');
+      testimonials[currentTestimonial].classList.add('fadeInRight');
+    }, 250);
+  }, 5000);
+};
