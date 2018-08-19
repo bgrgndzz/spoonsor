0fe3f5ccf0cca0d0c69c4512a9e1c32101c873f3
Buğra Gündüz <bgrgndzz@gmail.com>
2018-08-20 02:18:40 +0300

    create User model

---
+++ models/User/User.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  // required
+  auth: {
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  },
+  person: {
+    name: {
+      type: String,
+      required: true
+    },
+    surname: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String, 
+      required: true
+    },
+    phone: {
+      type: String, 
+      required: true
+    }
+  },
+  user: {
+    // shared
+    name: {
+      type: String, 
+      required: true
+    },
+    userType: {
+      type: String, 
+      required: true
+    },
+    profilepicture: {
+      type: String, 
+      required: true
+    },
+    active: {
+      type: Boolean, 
+      required: true, 
+      default: false
+    },
+    // seeker/shared
+    start: Date,
+    end: Date,
+    sponsorshipType: String,
+    seekerSubject: String,
+    // seeker/etkinlik
+    place: String,
+    etkinlikType: String
+  },
+  passwordReset: {
+    hash: String
+  }
+});
+
+module.exports = mongoose.model('User', UserSchema);
