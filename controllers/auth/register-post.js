935e59849efe855122503dbd1d373f334806547a
Buğra Gündüz <bgrgndzz@gmail.com>
2018-08-20 20:15:30 +0300

    receive post requests to /auth/register

---
+++ controllers/auth/register-post.js
@@ -0,0 +1,5 @@
+const User = require('../../models/User/User');
+
+module.exports = (req, res, next) => {
+  console.log(req.body);
+};
--- routes/auth.js
+++ routes/auth.js
@@ -5,10 +5,16 @@ const router = express.Router();
 const loginGetController = require('../controllers/auth/login-get');
 const registerGetController = require('../controllers/auth/register-get');
 
+const registerPostController = require('../controllers/auth/register-post');
+
+// require validators
+const registerPostValidator = require('../controllers/auth/register-validate');
+
 // routing
 // get
 router.get('/login', loginGetController);
 router.get('/register', registerGetController);
 // post
+router.post('/register', registerPostValidator, registerPostController);
 
 module.exports = router;
