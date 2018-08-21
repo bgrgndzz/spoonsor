a7b68dfc26f64dcc23a2737b1f432af6bbb3df80
Buğra Gündüz <bgrgndzz@gmail.com>
2018-08-22 02:32:13 +0300

    display seeker info in the modal

--- public/js/seekers.js
+++ public/js/seekers.js
@@ -1,3 +1,23 @@
+const changeModal = (data, modalWrapper) => {
+  const modalImage = modalWrapper.querySelector('.modal-image');
+  const modalTitle = modalWrapper.querySelector('.modal-title');
+  const modalSponsorships = modalWrapper.querySelector('.modal-sponsorships');
+  const modalDescription = modalWrapper.querySelector('.modal-description');
+
+  modalImage.style.backgroundImage = `url('/res/uploads/${data.profilepicture}')`;
+  modalTitle.innerHTML = data.name;
+  modalDescription.innerHTML = data.description || '';
+  modalSponsorships.innerHTML = '';
+  
+  data.sponsorshipType.forEach((sponsorship) => {
+    const sponsorshipNode = document.createElement('span');
+    sponsorshipNode.classList.add('modal-sponsorship');
+    sponsorshipNode.innerHTML = sponsorship;
+
+    modalSponsorships.appendChild(sponsorshipNode);
+  });
+};
+
 window.onload = () => {
   const hamburger = document.querySelector('.hamburger');
   const hamburgerIcon = document.querySelector('.hamburger-icon');
@@ -16,7 +36,13 @@ window.onload = () => {
     }
   };
   openModalButtons.forEach(openModalButton => {
-    openModalButton.onclick = () => toggleDisplay(modalWrapper, true);
+    openModalButton.onclick = (event) => {
+      const itemId = event.target.closest('.item').querySelector('.item-id').value;
+      const item = users.find(user => user.id === itemId);
+
+      changeModal(item, modalWrapper);
+      toggleDisplay(modalWrapper, true);
+    };
   });
   closeModalButtons.forEach(closeModalButton => {
     closeModalButton.onclick = () => toggleDisplay(modalWrapper, false);
