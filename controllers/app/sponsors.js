b8c3e2cdb0af55ce32ee6f27c7b388aa163c9890
Buğra Gündüz <bgrgndzz@gmail.com>
2018-08-23 01:48:55 +0300

    display sponsors

--- controllers/app/sponsors.js
+++ controllers/app/sponsors.js
@@ -1,14 +1,25 @@
+const mongoose = require('mongoose');
+const User = require('../../models/User/User');
+
 module.exports = (req, res, next) => {
-  res.render('app/sponsors', {
-    page: 'sponsors',
-    title: 'Sponsorlar',
-    includes: {
-      external: ['fontawesome'],
-      js: [
-        'sponsors',
-        'utils/toggleDisplay'
-      ]
-    },
-    headerType: 'app'
+  User.find().byType('sponsor').exec((err, users) => {
+    if (err) return res.status(500).send(err);
+
+    res.render('app/sponsors', {
+      page: 'sponsors',
+      title: 'Sponsorlar',
+      includes: {
+        external: ['fontawesome'],
+        js: [
+          'sponsors',
+          'utils/toggleDisplay'
+        ]
+      },
+      headerType: 'app',
+      users: users.map(user => ({
+        ...user.user,
+        id: user.id,
+      }))
+    });
   });
 };
