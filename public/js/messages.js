de70a40dcff77dca835d00437e12b6da985f0a09
Buğra Gündüz <bgrgndzz@gmail.com>
2018-08-24 17:52:20 +0300

    display user info in messages view

--- public/js/messages.js
+++ public/js/messages.js
@@ -1,22 +1,3 @@
-const changeModal = (data, modalWrapper) => {
-  const modalImage = modalWrapper.querySelector('.modal-image');
-  const modalTitle = modalWrapper.querySelector('.modal-title');
-  const modalSponsorships = modalWrapper.querySelector('.modal-sponsorships');
-  const modalDescription = modalWrapper.querySelector('.modal-description');
-
-  modalImage.style.backgroundImage = `url('/res/uploads/${data.profilepicture}')`;
-  modalTitle.innerHTML = data.name;
-  modalDescription.innerHTML = data.description || '';
-  modalSponsorships.innerHTML = '';
-  
-  data.sponsorshipType.forEach((sponsorship) => {
-    const sponsorshipNode = document.createElement('span');
-    sponsorshipNode.classList.add('modal-sponsorship');
-    sponsorshipNode.innerHTML = sponsorship;
-
-    modalSponsorships.appendChild(sponsorshipNode);
-  });
-};
 const resetMessages = () => {
   const messagesWrapper = document.querySelector('.messages-wrapper');
   messagesWrapper.innerHTML = '';
@@ -31,108 +12,167 @@ const loadMessages = (user) => {
   fetch('/app/messages/' + user)
     .then(data => data.json())
     .then(data => {
-      const content = document.querySelector('.content');
-  
-      const messagesWrapper = document.querySelector('.messages-wrapper');
-      messagesWrapper.innerHTML = '';
-
-      // messages header
-      const messagesHeader = document.createElement('div');
-      messagesHeader.classList.add('messages-header');
-      
-      const closeMessages = document.createElement('i');
-      closeMessages.classList.add('fas', 'fa-chevron-left', 'close-messages');
-      closeMessages.onclick = () => {
-        const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-        if (vw <= 700) {
-          content.style.marginLeft = '0';
-          document.querySelector('.user__active').classList.remove('user__active');
-          setTimeout(resetMessages, 500);
-        }
-      }
-
-      const messagesUser = document.createElement('h2');
-      messagesUser.classList.add('messages-user', 'open-modal');
-      messagesUser.innerHTML = 'Sponsor';
-
-      messagesHeader.appendChild(closeMessages);
-      messagesHeader.appendChild(messagesUser);
-
-      // messages
-      const messages = document.createElement('div');
-      messages.classList.add('messages');
-
-      data.messages.forEach(message => {
-        const messageNode = document.createElement('div');
-        messageNode.classList.add('message', 'message__' + message.type);
-
-        const messageImage = document.createElement('div');
-        messageImage.classList.add('message-image');
-        messageImage.style.backgroundImage = `url('/res/uploads/${message.user.profilepicture}')`;
-
-        const messageContent = document.createElement('div');
-        messageContent.classList.add('message-content');
-
-        const messageDetails = document.createElement('div');
-        messageDetails.classList.add('message-details');
-
-        const messageSender = document.createElement('span');
-        messageSender.classList.add('message-sender');
-        messageSender.innerHTML = message.user.name;
-
-        const messageDate = document.createElement('span');
-        messageDate.classList.add('message-date');
-        messageDate.innerHTML = message.date;
-
-        messageDetails.appendChild(messageSender);
-        messageDetails.appendChild(messageDate);
-
-        const messageText = document.createElement('p');
-        messageText.classList.add('message-text');
-        messageText.innerHTML = message.message;
-
-        messageContent.appendChild(messageDetails);
-        messageContent.appendChild(messageText);
-
-        messageNode.appendChild(messageImage);
-        messageNode.appendChild(messageContent);
+      // messages wrapper
+      (() => {
+        const content = document.querySelector('.content');
         
-        messages.appendChild(messageNode);
-      });
+        const messagesWrapper = document.querySelector('.messages-wrapper');
+        messagesWrapper.innerHTML = '';
 
-      // message form
-      const messageForm = document.createElement('form');
-      messageForm.classList.add('message-form');
+        // messages header
+        const messagesHeader = document.createElement('div');
+        messagesHeader.classList.add('messages-header');
+        
+        const closeMessages = document.createElement('i');
+        closeMessages.classList.add('fas', 'fa-chevron-left', 'close-messages');
+        closeMessages.onclick = () => {
+          const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+          if (vw <= 700) {
+            content.style.marginLeft = '0';
+            document.querySelector('.user__active').classList.remove('user__active');
+            setTimeout(resetMessages, 500);
+          }
+        }
 
-      const messageField = document.createElement('textarea');
-      messageField.classList.add('field', 'message-field');
-      messageField.name = 'message';
-      messageField.placeholder = 'Bir mesaj yazın';
+        const messagesUser = document.createElement('h2');
+        messagesUser.classList.add('messages-user', 'open-modal');
+        messagesUser.innerHTML = 'Sponsor';
 
-      const messageButton = document.createElement('input');
-      messageButton.classList.add('button', 'message-button');
-      messageButton.type = 'submit';
-      messageButton.value = '';
+        messagesHeader.appendChild(closeMessages);
+        messagesHeader.appendChild(messagesUser);
 
-      messageForm.appendChild(messageField);
-      messageForm.appendChild(messageButton);
+        // messages
+        const messages = document.createElement('div');
+        messages.classList.add('messages');
 
-      // append everything to the wrapper
-      messagesWrapper.appendChild(messagesHeader);
-      messagesWrapper.appendChild(messages);
-      messagesWrapper.appendChild(messageForm);
+        data.messages.forEach(message => {
+          const messageNode = document.createElement('div');
+          messageNode.classList.add('message', 'message__' + message.type);
 
-      messagesWrapper.scrollTop = messagesWrapper.scrollHeight;
+          const messageImage = document.createElement('div');
+          messageImage.classList.add('message-image');
+          messageImage.style.backgroundImage = `url('/res/uploads/${message.user.profilepicture}')`;
+
+          const messageContent = document.createElement('div');
+          messageContent.classList.add('message-content');
+
+          const messageDetails = document.createElement('div');
+          messageDetails.classList.add('message-details');
+
+          const messageSender = document.createElement('span');
+          messageSender.classList.add('message-sender');
+          messageSender.innerHTML = message.user.name;
+
+          const messageDate = document.createElement('span');
+          messageDate.classList.add('message-date');
+          messageDate.innerHTML = message.date;
+
+          messageDetails.appendChild(messageSender);
+          messageDetails.appendChild(messageDate);
+
+          const messageText = document.createElement('p');
+          messageText.classList.add('message-text');
+          messageText.innerHTML = message.message;
+
+          messageContent.appendChild(messageDetails);
+          messageContent.appendChild(messageText);
+
+          messageNode.appendChild(messageImage);
+          messageNode.appendChild(messageContent);
+          
+          messages.appendChild(messageNode);
+        });
+
+        // message form
+        const messageForm = document.createElement('form');
+        messageForm.classList.add('message-form');
+
+        const messageField = document.createElement('textarea');
+        messageField.classList.add('field', 'message-field');
+        messageField.name = 'message';
+        messageField.placeholder = 'Bir mesaj yazın';
+
+        const messageButton = document.createElement('input');
+        messageButton.classList.add('button', 'message-button');
+        messageButton.type = 'submit';
+        messageButton.value = '';
+
+        messageForm.appendChild(messageField);
+        messageForm.appendChild(messageButton);
+
+        // append everything to the wrapper
+        messagesWrapper.appendChild(messagesHeader);
+        messagesWrapper.appendChild(messages);
+        messagesWrapper.appendChild(messageForm);
+
+        messagesWrapper.scrollTop = messagesWrapper.scrollHeight;
+      })();
+
+      // user wrapper
+      (() => {
+        const userWrapper = document.querySelector('.user-wrapper');
+        userWrapper.innerHTML = '';
+        
+        const userContent = document.createElement('div');
+        userContent.classList.add('user-content');
+
+        const userImage = document.createElement('div');
+        userImage.classList.add('user-image');
+        userImage.style.backgroundImage = `url('/res/uploads/${data.user.profilepicture}')`;
+
+        const userName = document.createElement('h2');
+        userName.classList.add('user-name');
+        userName.innerHTML = data.user.name;
+
+        const userDescription = document.createElement('p');
+        userDescription.classList.add('user-description');
+        userDescription.innerHTML = data.user.description || '';
+
+        const userSponsorships = document.createElement('div');
+        userSponsorships.classList.add('user-sponsorships');
+
+        data.user.sponsorshipType.forEach(userSponsorship => {
+          const userSponsorshipNode = document.createElement('span');
+          userSponsorshipNode.classList.add('user-sponsorship');
+          userSponsorshipNode.innerHTML = userSponsorship;
+
+          userSponsorships.appendChild(userSponsorshipNode);
+        });
+
+        userContent.appendChild(userImage);
+        userContent.appendChild(userName);
+        userContent.appendChild(userDescription);
+        userContent.appendChild(userSponsorships);
+
+        userWrapper.appendChild(userContent);
+      })();
+
+      // user modal
+      (() => {
+        const modalImage = document.querySelector('.modal-image');
+        modalImage.style.backgroundImage = `url('/res/uploads/${data.user.profilepicture}')`;
+
+        const modalTitle = document.querySelector('.modal-title');
+        modalTitle.innerHTML = data.user.name;
+
+        const modalDescription = document.querySelector('.modal-description');
+        modalDescription.innerHTML = data.user.description || '';
+
+        const modalSponsorships = document.querySelector('.modal-sponsorships');
+        data.user.sponsorshipType.forEach(modalSponsorship => {
+          const modalSponsorshipNode = document.createElement('span');
+          modalSponsorshipNode.classList.add('user-sponsorship');
+          modalSponsorshipNode.innerHTML = modalSponsorship;
+
+          modalSponsorships.appendChild(modalSponsorshipNode);
+        });
+      })();
     });
 };
 
 window.onload = () => {
   const hamburger = document.querySelector('.hamburger');
   const hamburgerIcon = document.querySelector('.hamburger-icon');
-
-  const modalWrapper = document.querySelector('.modal-wrapper');
-  const openModalButtons = document.querySelectorAll('.open-modal');
-  const closeModalButtons = document.querySelectorAll('.close-modal');
 
   const users = document.querySelectorAll('.users-wrapper .user');
   const content = document.querySelector('.content');
@@ -149,14 +189,16 @@ window.onload = () => {
       document.body.style.overflow = 'hidden';
     }
   };
-  openModalButtons.forEach(openModalButton => {
-    openModalButton.onclick = (event) => {
+
+  document.addEventListener('click', (event) => {
+    const modalWrapper = document.querySelector('.modal-wrapper');
+    if (event.target && event.target.classList.contains('open-modal')) {
       toggleDisplay(modalWrapper, true);
-    };
+    }else if (event.target && event.target.classList.contains('close-modal')) {
+      toggleDisplay(modalWrapper, false);
+    }
   });
-  closeModalButtons.forEach(closeModalButton => {
-    closeModalButton.onclick = () => toggleDisplay(modalWrapper, false);
-  });
+
   users.forEach(user => {
     user.onclick = () => {
       if (!user.classList.contains('user__active')) {
