3012fb96f3f1913df03042c9ec50af3a23bcf2b3
Buğra Gündüz <bgrgndzz@gmail.com>
2018-08-24 15:31:11 +0300

    load messages ui when a user is chosen from the sidebar

--- public/js/messages.js
+++ public/js/messages.js
@@ -17,6 +17,70 @@ const changeModal = (data, modalWrapper) => {
     modalSponsorships.appendChild(sponsorshipNode);
   });
 };
+const resetMessages = () => {
+  const messagesWrapper = document.querySelector('.messages-wrapper');
+  messagesWrapper.innerHTML = '';
+
+  const messagesBackgroundNotice = document.createElement('span');
+  messagesBackgroundNotice.classList.add('messages-background-notice');
+  messagesBackgroundNotice.innerHTML = 'Mesajları görüntülemek için sol taraftan bir görüşme seçin';
+
+  messagesWrapper.appendChild(messagesBackgroundNotice);
+};
+const loadMessages = (user, data) => {
+  const content = document.querySelector('.content');
+  
+  const messagesWrapper = document.querySelector('.messages-wrapper');
+  messagesWrapper.innerHTML = '';
+
+  // messages header
+  const messagesHeader = document.createElement('div');
+  messagesHeader.classList.add('messages-header');
+  
+  const closeMessages = document.createElement('i');
+  closeMessages.classList.add('fas', 'fa-chevron-left', 'close-messages');
+  closeMessages.onclick = () => {
+    const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    if (vw <= 700) {
+      content.style.marginLeft = '0';
+      document.querySelector('.user__active').classList.remove('user__active');
+      setTimeout(resetMessages, 500);
+    }
+  }
+
+  const messagesUser = document.createElement('h2');
+  messagesUser.classList.add('messages-user', 'open-modal');
+  messagesUser.innerHTML = 'Sponsor';
+
+  messagesHeader.appendChild(closeMessages);
+  messagesHeader.appendChild(messagesUser);
+
+  // messages
+  const messages = document.createElement('div');
+  messages.classList.add('messages');
+
+  // message form
+  const messageForm = document.createElement('form');
+  messageForm.classList.add('message-form');
+
+  const messageField = document.createElement('textarea');
+  messageField.classList.add('field', 'message-field');
+  messageField.name = 'message';
+  messageField.placeholder = 'Bir mesaj yazın';
+
+  const messageButton = document.createElement('input');
+  messageButton.classList.add('button', 'message-button');
+  messageButton.type = 'submit';
+  messageButton.value = '';
+
+  messageForm.appendChild(messageField);
+  messageForm.appendChild(messageButton);
+
+  // append everything to the wrapper
+  messagesWrapper.appendChild(messagesHeader);
+  messagesWrapper.appendChild(messages);
+  messagesWrapper.appendChild(messageForm);
+};
 
 window.onload = () => {
   const hamburger = document.querySelector('.hamburger');
@@ -27,7 +91,6 @@ window.onload = () => {
   const closeModalButtons = document.querySelectorAll('.close-modal');
 
   const users = document.querySelectorAll('.users-wrapper .user');
-  const closeMessages = document.querySelector('.close-messages');
   const content = document.querySelector('.content');
   
   const socket = io();
@@ -53,22 +116,22 @@ window.onload = () => {
   users.forEach(user => {
     user.onclick = () => {
       const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+      if (document.querySelector('.user__active')) {
+        document.querySelector('.user__active').classList.remove('user__active');
+      }
+      setTimeout(() => user.classList.add('user__active'), vw <= 700 ? 500 : 0);
+      loadMessages();
       if (vw <= 700) {
         content.style.marginLeft = '-100vw';
-        window.onresize = () => {
-          const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-          if (vw > 700) {
-            content.style.marginLeft = '0';
-          }
-        };
       }
     };
   });
-  closeMessages.onclick = () => {
+
+  window.onresize = () => {
     const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    if (vw <= 700) {
+    if (vw > 700) {
       content.style.marginLeft = '0';
-      window.onresize = () => {}; 
     }
-  }
+  };
 };
