842ca9e6a9ecb643fe2b789268ac6be57f04e699
Buğra Gündüz <bgrgndzz@gmail.com>
2018-08-30 18:00:47 +0300

    hide self in events and sponsors

--- controllers/app/events.js
+++ controllers/app/events.js
@@ -2,24 +2,27 @@ const mongoose = require('mongoose');
 const User = require('../../models/User/User');
 
 module.exports = (req, res, next) => {
-  User.find().byType('etkinlik').exec((err, users) => {
-    if (err) return res.status(500).send(err);
+  User
+    .find({ _id: { $ne: req.session.user.id } })
+    .byType('etkinlik')
+    .exec((err, users) => {
+      if (err) return res.status(500).send(err);
 
-    res.render('app/events', {
-      page: 'events',
-      title: 'Etkinlikler',
-      includes: {
-        external: ['fontawesome'],
-        js: [
-          'events',
-          'utils/toggleDisplay'
-        ]
-      },
-      headerType: 'app',
-      users: users.map(user => ({
-        ...user.user,
-        id: user.id,
-      }))
+      res.render('app/events', {
+        page: 'events',
+        title: 'Etkinlikler',
+        includes: {
+          external: ['fontawesome'],
+          js: [
+            'events',
+            'utils/toggleDisplay'
+          ]
+        },
+        headerType: 'app',
+        users: users.map(user => ({
+          ...user.user,
+          id: user.id,
+        }))
+      });
     });
-  });
 };
--- controllers/app/sponsors.js
+++ controllers/app/sponsors.js
@@ -2,24 +2,27 @@ const mongoose = require('mongoose');
 const User = require('../../models/User/User');
 
 module.exports = (req, res, next) => {
-  User.find().byType('sponsor').exec((err, users) => {
-    if (err) return res.status(500).send(err);
+  User
+    .find({ _id: { $ne: req.session.user.id } })
+    .byType('sponsor')
+    .exec((err, users) => {
+      if (err) return res.status(500).send(err);
 
-    res.render('app/sponsors', {
-      page: 'sponsors',
-      title: 'Sponsorlar',
-      includes: {
-        external: ['fontawesome'],
-        js: [
-          'sponsors',
-          'utils/toggleDisplay'
-        ]
-      },
-      headerType: 'app',
-      users: users.map(user => ({
-        ...user.user,
-        id: user.id,
-      }))
+      res.render('app/sponsors', {
+        page: 'sponsors',
+        title: 'Sponsorlar',
+        includes: {
+          external: ['fontawesome'],
+          js: [
+            'sponsors',
+            'utils/toggleDisplay'
+          ]
+        },
+        headerType: 'app',
+        users: users.map(user => ({
+          ...user.user,
+          id: user.id,
+        }))
+      });
     });
-  });
 };
