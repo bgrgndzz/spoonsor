405a223e401c399ac17deace78ed19f3cc0b0887
Buğra Gündüz <bgrgndzz@gmail.com>
2018-09-04 02:47:59 +0300

    fetch data from database for the landing page

--- controllers/index/index.js
+++ controllers/index/index.js
@@ -1,14 +1,30 @@
+const mongoose = require('mongoose');
+const User = require('../../models/User/User');
+
+const async = require('async');
+
 module.exports = (req, res, next) => {
-  res.render('index', {
-    page: 'index',
-    title: 'Sponsor Bulmanın En Kolay Yolu',
-    includes: {
-      external: [
-        'animate.css',
-        'fontawesome'
-      ],
-      js: ['index']
+  async.parallel({
+    sponsors: callback => {
+      User.countDocuments({'user.userType': 'sponsor'}, callback);
     },
-    headerType: 'landing'
+    events: callback => {
+      User.countDocuments({'user.userType': 'etkinlik'}, callback);
+    }
+  }, (err, data) => {
+    if (err) return console.log(err);
+    res.render('index', {
+      page: 'index',
+      title: 'Sponsor Bulmanın En Kolay Yolu',
+      includes: {
+        external: [
+          'animate.css',
+          'fontawesome'
+        ],
+        js: ['index']
+      },
+      headerType: 'landing',
+      data
+    });
   });
 };
