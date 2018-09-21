e9be660afcb0d95952d3c379436bd5c4e4ee5c2f
Buğra Gündüz <bgrgndzz@gmail.com>
2018-09-21 17:16:32 +0300

    automatically send data to airtable

--- controllers/auth/register/post.js
+++ controllers/auth/register/post.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const User = require('../../../models/User/User');
 
+const airtable = require('airtable');
+airtable.configure({
+  endpointUrl: 'https://api.airtable.com',
+  apiKey: process.env.AIRTABLE_API_KEY
+});
+const base = airtable.base('appeBo7sITVab1nYA');
+
 const sendMail = require('../../../utils/sendMail');
 
 module.exports = (req, res, next) => {
@@ -32,19 +39,31 @@ module.exports = (req, res, next) => {
   const newUser = new User(newUserData);
   newUser.save(err => {
     if (err) return console.log(err);
-    
-    req.session.user = {
-      ...newUser.user,
-      email: newUser.person.email,
-      id: newUser._id
-    };
 
-    sendMail({
-      name: newUser.user.name,
-      email: newUser.person.email
-    }, 'etkinlikRegister', (err, info) => {
+    base('Üyeler').create({
+      'İsim': req.body.etkinlikname,
+      'Ad': req.body.name,
+      'Soyad': req.body.surname,
+      'Email': req.body.email,
+      'Telefon': req.body.phone,
+      'Tip': ['etkinlik'],
+      'Giriş sayısı': 1
+    }, (err, record) => {
       if (err) return console.log(err);
-      res.redirect('/app');
+
+      req.session.user = {
+        ...newUser.user,
+        email: newUser.person.email,
+        id: newUser._id
+      };
+  
+      sendMail({
+        name: newUser.user.name,
+        email: newUser.person.email
+      }, 'etkinlikRegister', (err, info) => {
+        if (err) return console.log(err);
+        res.redirect('/app');
+      });
     });
   });
 };
--- controllers/auth/sponsor-register/post.js
+++ controllers/auth/sponsor-register/post.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const User = require('../../../models/User/User');
 
+const airtable = require('airtable');
+airtable.configure({
+  endpointUrl: 'https://api.airtable.com',
+  apiKey: process.env.AIRTABLE_API_KEY
+});
+const base = airtable.base('appeBo7sITVab1nYA');
+
 const sendMail = require('../../../utils/sendMail');
 
 module.exports = (req, res, next) => {
@@ -28,18 +35,30 @@ module.exports = (req, res, next) => {
   newUser.save(err => {
     if (err) return res.status(500).send(err);
     
-    req.session.user = {
-      ...newUser.user,
-      email: newUser.person.email,
-      id: newUser._id
-    };
-
-    sendMail({
-      name: newUser.user.name,
-      email: newUser.person.email
-    }, 'sponsorRegister', (err, info) => {
+    base('Üyeler').create({
+      'İsim': req.body.sponsorname,
+      'Ad': req.body.name,
+      'Soyad': req.body.surname,
+      'Email': req.body.email,
+      'Telefon': req.body.phone,
+      'Tip': ['sponsor'],
+      'Giriş sayısı': 1
+    }, (err, record) => {
       if (err) return console.log(err);
-      res.redirect('/app');
+
+      req.session.user = {
+        ...newUser.user,
+        email: newUser.person.email,
+        id: newUser._id
+      };
+  
+      sendMail({
+        name: newUser.user.name,
+        email: newUser.person.email
+      }, 'sponsorRegister', (err, info) => {
+        if (err) return console.log(err);
+        res.redirect('/app');
+      });
     });
   });
 };
